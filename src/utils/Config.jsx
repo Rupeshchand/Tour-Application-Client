6c55733942120c5449ad56d08339adacf019f552
Rupesh Chand <rupeshchandvasantam@gmail.com>
2025-04-07 17:35:16 +0530

    added backend url to config file

--- src/utils/Config.jsx
+++ src/utils/Config.jsx
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:5000/api"
-export const BASE_URL = "https://tour-application-w4cs.onrender.com/api"
+export const BASE_URL = "https://tour-application-lall.onrender.com"
